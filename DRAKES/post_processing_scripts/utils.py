7d96912229141cb81512ba824b5a3894833c7b42
Shai Dickman <shai.dickman@berkeley.edu>
2025-07-23 23:03:13 -0700

    supporting all data set loading

--- DRAKES/post_processing_scripts/utils.py
+++ DRAKES/post_processing_scripts/utils.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 from protein_oracle.data_utils import ProteinStructureDataset, ProteinDPODataset, featurize
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, ConcatDataset
 import pickle
 
 def process_seq_data_csv(fn, target_col, process_fn):
@@ -16,7 +16,7 @@ def process_seq_data_csv(fn, target_col, process_fn):
             df[target_col] = value
             df.to_csv(fn, index=False)
     except Exception as e: 
-        print(e)
+        print(f"ERROR: {e}")
         pass
 
 def process_seq_data_directory(dir_name, target_col, process_fn):
@@ -28,19 +28,6 @@ def process_seq_data_directory(dir_name, target_col, process_fn):
         file_path = os.path.join(dir_name, fn)
         if fn.lower().endswith(".csv"):
             process_seq_data_csv(file_path, target_col, process_fn)
-            # print(f"Processing {fn}...")
-            # try:
-            #     df = pd.read_csv(file_path, nrows=0)  # Read only the header
-            #     if target_col in df.columns: # Don't re-compute if already done
-            #         print(f"{fn} already processed - Skipping...")
-            #     else:
-            #         df = pd.read_csv(file_path)
-            #         value = process_fn(df)
-            #         df[target_col] = value
-            #         df.to_csv(file_path, index=False)
-            # except Exception as e: 
-            #     print(e)
-            #     pass
 
 def get_drakes_test_data():
     base_path = "DRAKES/data/data_and_model"
@@ -54,7 +41,16 @@ def get_drakes_test_data():
         pdb_idx_dict = {pdb_filenames[i]: i for i in range(len(pdb_filenames))}
         break
     dpo_dict_path = os.path.join(base_path, 'proteindpo_data/processed_data')
+    
     dpo_test_dict = pickle.load(open(os.path.join(dpo_dict_path, 'dpo_test_dict_wt.pkl'), 'rb'))
-    dpo_test_dataset = ProteinDPODataset(dpo_test_dict, pdb_idx_dict, pdb_structures)
-    loader_test = DataLoader(dpo_test_dataset, batch_size=1, shuffle=False)
+    dpo_valid_dict = pickle.load(open(os.path.join(dpo_dict_path, 'dpo_valid_dict_wt.pkl'), 'rb'))
+    dpo_train_dict = pickle.load(open(os.path.join(dpo_dict_path, 'dpo_train_dict_wt.pkl'), 'rb'))
+
+    dpo_test_dataset = ProteinDPODataset(dpo_test_dict, pdb_idx_dict, pdb_structures) #type: ignore
+    dpo_valid_dataset = ProteinDPODataset(dpo_valid_dict, pdb_idx_dict, pdb_structures) #type: ignore
+    dpo_train_dataset = ProteinDPODataset(dpo_train_dict, pdb_idx_dict, pdb_structures) #type: ignore
+
+    combined_dataset = ConcatDataset([dpo_test_dataset, dpo_valid_dataset, dpo_train_dataset])
+
+    loader_test = DataLoader(combined_dataset, batch_size=1, shuffle=False)
     return base_path, pdb_path, loader_test
